437dae3cab4110ddaf96ef06598a0f11c943f1f8
iremugurlu <iremugurlu8l@gmail.com>
2019-11-17 11:43:40 +0100

    initial commit

---
+++ app/Http/Controllers/Seller/SellerController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Seller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class SellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sellers = Seller::has('products')->get();
+
+        return $this->showAll($sellers);
+        //return response()->json(['data'=>$sellers], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Seller $seller) {
+
+        return $this->showOne($seller);
+        //return response()->json(['data'=>$seller], 200);
+    }
+}
