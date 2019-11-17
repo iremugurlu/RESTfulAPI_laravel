48e812ed90eaedb295235bb342cc8b35b48f2660
iremugurlu <iremugurlu8l@gmail.com>
2019-11-17 20:20:43 +0100

    add the email and verification feature

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\User;
 
 use App\User;
+use App\Mail\UserCreated;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\ApiController;
 
 class UserController extends ApiController
@@ -71,40 +73,35 @@ class UserController extends ApiController
     {
 
         $rules = [
-            'email' => 'email|unique:users, email' . $user->id,
+            'email' => 'email|unique:users,email,' . $user->id,
             'password' => 'min:6|confirmed',
             'admin' => 'in:' . User::ADMIN_USER . ',' . User::REGULAR_USER,
         ];
-
-        $this->validate($request, $rules);
-
-        if($request->has('name')) {
+        if ($request->has('name')) {
             $user->name = $request->name;
         }
-        if($request->has('email') && $user->email != $request->email) {
+        if ($request->has('email') && $user->email != $request->email) {
             $user->verified = User::UNVERIFIED_USER;
             $user->verification_token = User::generateVerificationCode();
             $user->email = $request->email;
         }
-        if($request->has('password')) {
+        if ($request->has('password')) {
             $user->password = bcrypt($request->password);
         }
-        if($request->has('admin')) {
-            if(! $user->isVerified()) {
-                return $this->errorResponse('Only verified users can modify the admin field :(', 409);
-            } 
-
+        if ($request->has('admin')) {
+            $this->allowedAdminAction();
+        
+            if (!$user->isVerified()) {
+                return $this->errorResponse('Only verified users can modify the admin field', 409);
+            }
             $user->admin = $request->admin;
-            
         }
-
-        if($user->isDirty()) {
+        if (!$user->isDirty()) {
             return $this->errorResponse('You need to specify a different value to update', 422);
-        } 
-
+        }
         $user->save();
-
         return $this->showOne($user);
+
         
     }
 
@@ -119,4 +116,29 @@ class UserController extends ApiController
         $user->delete();
         return $this->showOne($user);
     }
+
+    public function verify($token) {
+        $user = User::where('verification_token', $token)->firstOrFail();
+
+        $user->verified = User::VERIFIED_USER;
+        $user->verification_token = null;
+
+        $user->save();
+
+        return $this->showMessage('The account has been verified successfully');
+    }
+
+    public function resend(User $user) {
+        if($user->isVerified()) {
+            return $this->errorResponse('This user is already verified', 409);
+        }
+
+        retry(5, function() use ($user) {
+                Mail::to($user)->send(new UserCreated($user));
+            }, 100);
+
+        return $this->showMessage('The verification email has resent');
+
+    }
+
 }
