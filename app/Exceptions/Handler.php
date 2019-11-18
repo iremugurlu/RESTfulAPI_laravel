f3c9e190dfd68eb9ba0616afdaa733091115ce27
iremugurlu <iremugurlu8l@gmail.com>
2019-11-18 20:55:43 +0100

    add authentication

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use Exception;
 use App\Traits\ApiResponser;
 use Illuminate\Database\QueryException;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -93,6 +94,10 @@ class Handler extends ExceptionHandler
             }
         }
 
+        if($exception instanceof TokenMismatchException) {
+            return redirect()->back()->withInput($request->input());
+        }
+
         if(config('app.debug')) {
             return parent::render($request, $exception);
         }
@@ -102,6 +107,10 @@ class Handler extends ExceptionHandler
     }
 
     protected function unauthenticated($request, AuthenticationException $exception) {
+
+        if ($this->isFrontend($request)) {
+            return redirect()->guest('login');
+        }
 
         return $this->errorResponse('Unauthenticated', 401);
    
@@ -118,6 +127,14 @@ class Handler extends ExceptionHandler
     {
         $errors = $e->validator->errors()->getMessages();
         
+        if ($this->isFrontend($request)) {
+            return $request->ajax() ? reponse()->json($errors, 422) : redirect()->back()->withInput($request->input())->withErrors($errors);
+        }
+
         return $this->errorResponse($errors, 422);
+    }
+
+    private function isFrontend($request) {
+        return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,23 @@
 */
 
 
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+if ($options['reset'] ?? true) {
+    Route::resetPassword();
+}
+
+// Email Verification Routes...
+if ($options['verify'] ?? false) {
+    Route::emailVerification();
+}
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function() {
+	return view('welcome');
+})->middleware('guest');
